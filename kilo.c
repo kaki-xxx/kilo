391e2f88f04e363d7df35cfdc7959a97407a3a5b
kaki <suka6411@gmail.com>
2019-10-26 15:35:12 +0900

    add: status message for save

--- kilo.c
+++ kilo.c
@@ -63,6 +63,10 @@ struct editorConfig {
 
 struct editorConfig E;
 
+/*** prototypes ***/
+
+void editorSetStatusMessage(const char *fmt, ...);
+
 /*** terminal ***/
 
 void die(const char *s) {
@@ -298,6 +302,7 @@ void editorSave() {
             if (write(fd, buf, len) == len) {
                 close(fd);
                 free(buf);
+                editorSetStatusMessage("%d bytes written to disk", len);
                 return;
             }
         }
@@ -305,6 +310,7 @@ void editorSave() {
     }
 
     free(buf);
+    editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 /*** append buffer ***/
@@ -577,7 +583,7 @@ int main(int argc, char *argv[]) {
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
     while (1) {
         editorProcessKeyPress();
